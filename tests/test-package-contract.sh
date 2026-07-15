38170054658482bedd163f5c0849fd6ba964fd58
HoshimiRIN <admin@sakuramoe.dev>
2026-07-16 05:37:14 +0800

    ci: install all SDK package feeds

--- tests/test-package-contract.sh
+++ tests/test-package-contract.sh
@@ -5,6 +5,7 @@ ROOT="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
 ACL="$ROOT/root/usr/share/rpcd/acl.d/luci-app-usb-modem.json"
 MENU="$ROOT/root/usr/share/luci/menu.d/luci-app-usb-modem.json"
 VIEW="$ROOT/htdocs/luci-static/resources/view/usbmodem/status.js"
+WORKFLOW="$ROOT/.github/workflows/build-ipk.yml"
 
 node -e "JSON.parse(require('fs').readFileSync(process.argv[1], 'utf8'))" "$ACL"
 node -e "JSON.parse(require('fs').readFileSync(process.argv[1], 'utf8'))" "$MENU"
@@ -18,6 +19,8 @@ for helper in /usr/bin/usbmodem-status /usr/bin/usbmodem-restart; do
 done
 
 grep -q '^LUCI_DEPENDS:=.*+rpcd-mod-file' "$ROOT/Makefile"
+grep -Fq './scripts/feeds update -a' "$WORKFLOW"
+grep -Fq './scripts/feeds install -a' "$WORKFLOW"
 awk -F= '
 	/^PKG_RELEASE:=/ {
 		found = 1
